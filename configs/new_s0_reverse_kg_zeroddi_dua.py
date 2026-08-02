1f1f5a48296347d7ed02a804d58a2e7e23a14dc0
Wu Mengying <1733725203@qq.com>
2026-08-02 10:48:31 -0400

    Tune ZeroDDI DUA auxiliary experiments

--- configs/new_s0_reverse_kg_zeroddi_dua.py
+++ configs/new_s0_reverse_kg_zeroddi_dua.py
@@ -1,7 +1,9 @@
 _base_ = './new_s0_reverse_kg.py'
 
 model = dict(
-    zeroddi_dua_aux_lambda=0.1,
+    class_balanced_beta=0.0,
+    static_ddie_uniformity_lambda=0.0,
+    zeroddi_dua_aux_lambda=0.3,
 )
 
 work_dir = './work_dirs/new_s0_reverse_kg_zeroddi_dua'
